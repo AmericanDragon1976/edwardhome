71d9bf3b4d8d03e857c6c202cb002804674c87f1
Edward Moroni Goodson <bobby_83110@yahoo.com>
2013-12-06 12:17:03 -0700

    php script testing

---
+++ src/misc.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Edward Moroni Goodson personal home page</title>
+    <link rel="stylesheet" type="text/css" href="../css/global.css">
+  </head>
+  <body>
+  	<div id="pagecenter">
+      <div class="side"> <!-- left side bar--></div>
+      <div class="center"> 
+      	<div><!-- Title area -->
+          <h1>Misc.</h1>
+          <h2>Tagline.</h2>
+        </div>
+        <div class="center" style="background-color:#98bf21;> <!-- navigaton bar-->
+          <ul class="nav">
+            <li class="nav"> <a href="../index.html" class="nav"> Home </a> </li> 
+          	<li class="nav"> <a href="chess.html" class="nav"> Chess </a> </li>
+          	<li class="nav"> <a href="dnd.html" class="nav"> DnD </a> </li>
+          	<li class="nav"> <a href="religion.html" class="nav"> Religion </a> </li>
+          	<li class="nav"> <a href="programming.html" class="nav"> Programming </a> </li>
+          </ul>
+          <ul class="nav">
+          	<li class="nav"> <a href="poetry.html" class="nav"> Poetry </a> </li>
+          	<li class="nav"> <a href="recorder.html" class="nav"> Recorder </a> </li>
+          	<li class="nav"> <a href="personal.html" class="nav"> Personal </a> </li>
+            <li class="nav"> <a href="misc.php" class="nav"> Misc. </a> </li>
+          </ul>
+        </div> 
+        <div style="background-color:#ffff70"> <!-- Main page area-->
+          </br>
+          <p class="general">&nbsp&nbsp&nbsp This page is under condtruction!!</p>
+		  <p class="general">
+		  <?php 
+		     echo "Hello World!";
+		  ?>
+		  </p>
+          </br>
+        </div>
+      </div>
+      <div class="side"> <!-- right side bar--> </div>
+    </div>
+  </body>
+</html>
